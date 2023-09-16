7fc2c66e3a0d209c85f14266ac4a55986876a776
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-09-16 23:14:48 +0200

    geolocation button, marker icons

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -8,6 +8,12 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit, AfterViewInit{
 
+  @ViewChild('map') mapEl: ElementRef;
+  private map: L.Map;
+
+  private geoLocCircle: L.Circle;
+  private geoLocMarker: L.Marker;
+
   constructor() {}
 
   ngOnInit(): void {
@@ -15,12 +21,48 @@ export class MapComponent implements OnInit, AfterViewInit{
   }
 
   ngAfterViewInit(): void {
-    const map = L.map('map').setView([54.370978, 18.612741], 13).setZoom(18);
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map(this.mapEl.nativeElement).setView([54.370978, 18.612741], 13).setZoom(18);
+    
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(this.map);
 
+    L.control.scale().addTo(this.map);
+
+    L.Icon.Default.imagePath = 'assets/map/';
+
+    this.geoLocCircle = L.circle([0, 0], 0);
+    this.geoLocMarker = L.marker([0, 0]);
+
+  }
+
+  geolocationClick(): void {
+    navigator.geolocation.getCurrentPosition(
+      this.geolocationSuccess, 
+      this.geolocationError,
+      { enableHighAccuracy: true }
+    );
+  }
+
+  geolocationSuccess = (pos: GeolocationPosition): void => {
+    console.log('Geolocation success.', pos.coords);
+    const latLon = new L.LatLng(pos.coords.latitude, pos.coords.longitude);
+    this.map.setView(latLon, 13);
+    this.geoLocCircle.removeFrom(this.map).setLatLng(latLon).setRadius(pos.coords.accuracy).addTo(this.map);
+    this.geoLocMarker.removeFrom(this.map).setLatLng(latLon).addTo(this.map);
+  }
+
+  geolocationError = (err: GeolocationPositionError): void => {
+    // https://stackoverflow.com/questions/61351331/using-geolocation-getcurrentposition-while-testing-on-local-network#answer-61527822
+    console.error(err);
+    alert('Cannot read geolocation data');
+    this.geoLocCircle.removeFrom(this.map)
+    this.geoLocMarker.removeFrom(this.map)
   }
 
 }
