2c5b3db72235dbac9ce47be06747c78dc3e0c969
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-09-17 14:37:23 +0200

    ::ng-deep view encapsulation fix

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
