bb499023b7daeb48bd45ba2658a12a26b804ccab
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-09-21 21:47:15 +0200

    add production backend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   backendUrl: 'http://localhost:8080/api/v1',
   //backendUrl: 'http://192.168.1.14:8080/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  backendUrl: undefined,
+  production: true,
+  backendUrl: 'https://16.171.52.187:8080/api/v1',
 };
